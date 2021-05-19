4bb5299b08a7618347eb5c5eb2ebf222549060df
MyriamAb <myriam.abdelli@epitech.eu>
2021-05-19 14:52:49 +0200

    modules app

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -4,13 +4,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { User } from './users/user.entity';
 import { UsersModule } from './users/users.module';
+import { RentalModule } from './rental/rental.module';
+import { ReservationModule } from './reservation/reservation.module';
 
 @Module({
   imports: [
     UsersModule,
     TypeOrmModule.forRoot(
      
-    )
+    ),
+    RentalModule,
+    ReservationModule
   ],
   controllers: [AppController],
   providers: [AppService],
