65271c4fdf757a15250c111e9fb97d547ea408b2
Marion Langlet <marion.langlet@epitech.eu>
2021-05-20 12:49:20 +0200

    add checkbox atom

--- front/src/atoms/checkbox.js
+++ front/src/atoms/checkbox.js
@@ -3,7 +3,7 @@ import { Checkbox } from 'semantic-ui-react'
 
 function CheckboxType(props) {
   return (
-    <Checkbox label={props.label}  onClick={props.onClick} defaultChecked={props.defaultChecked} onChange={props.onChange} value={props.value} />
+    <Checkbox label={props.label} style={props.style} onClick={props.onClick} defaultChecked={props.defaultChecked} onChange={props.onChange} value={props.value} />
   )
 }
 export default CheckboxType
