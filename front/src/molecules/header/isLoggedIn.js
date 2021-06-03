06f925a9e5376080bfc484089896a8a0220542fc
alexandre <alexandre.de-framond@epitech.eu>
2021-06-03 13:06:20 +0200

    minor fix for link redirection

--- front/src/molecules/header/isLoggedIn.js
+++ front/src/molecules/header/isLoggedIn.js
@@ -16,8 +16,8 @@ export default function IsLoggedIn() {
                 <Dropdown.Menu>
                     <Dropdown.Item as ="a" href="/profile" onClick={(e) => userContext.profile(e, 0)}>Profile</Dropdown.Item>
                     <Dropdown.Divider />
-                    <Dropdown.Item as="a" href="/profile/" onClick={(e) => userContext.profile(e, 2)}>My ads</Dropdown.Item>
                     <Dropdown.Item as="a" href="/profile/" onClick={(e) => userContext.profile(e, 1)} >My booking</Dropdown.Item>
+                    <Dropdown.Item as="a" href="/profile/" onClick={(e) => userContext.profile(e, 2)}>My ads</Dropdown.Item>
                     <Dropdown.Item onClick={userContext.logout}> Logout </Dropdown.Item>
                 </Dropdown.Menu>
             </Dropdown>
